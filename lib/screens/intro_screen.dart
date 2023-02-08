40c45648f70e7367d9367567e48f0a601eecdc63
altrono <hbotema16@gmail.com>
2023-02-08 09:18:31 +0200

    Added TITLE

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app_ui/utils/colors.dart';
+import 'package:e_commerce_app_ui/utils/utils.dart';
 import 'package:e_commerce_app_ui/widgets/image_list_view.dart';
 import 'package:flutter/material.dart';
 
@@ -29,6 +30,16 @@ class _IntroScreenState extends State<IntroScreen> {
                 ],
               ),
            ),
+
+           //  title
+          Positioned(
+              top: MediaQuery.of(context).size.height * 0.08,
+              child: Text(
+                'ALTRONO IMPARA',
+                textAlign: TextAlign.center,
+                style: kTitleStyle,
+              ),
+          ),
 
         ],
       ),
---
+++ lib/utils/utils.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kTitleStyle = TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.5,
+  color: Colors.white,
+);
