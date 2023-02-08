b4c0dde7e30ee969dcc583ef7c71bc34fe426cf0
altrono <hbotema16@gmail.com>
2023-02-08 08:55:35 +0200

    Added CachedNetworkImage and ScrollController

--- lib/widgets/image_list_view.dart
+++ lib/widgets/image_list_view.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
 import 'dart:math';
 
+
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ImageListView extends StatefulWidget {
@@ -11,11 +15,74 @@ class ImageListView extends StatefulWidget {
 }
 
 class _ImageListViewState extends State<ImageListView> {
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (!_scrollController.position.atEdge) {
+         // implement scroll of list
+         _autoScroll();
+      }
+
+      //  adding to list
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        _autoScroll();
+      });
+    });
+
+  }
+
+  void _autoScroll() {
+    final currentScrollPosition = _scrollController.offset;
+    final scrollEndPosition = _scrollController.position.maxScrollExtent;
+    scheduleMicrotask(
+            () {
+              _scrollController.animateTo(
+                  currentScrollPosition == scrollEndPosition ? 0 : scrollEndPosition,
+                  duration: const Duration(seconds: 10),
+                  curve: Curves.linear,
+              );
+            }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Transform.rotate(
       angle: 1.96*pi,
-      child: SizedBox(),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.60,
+        height: MediaQuery.of(context).size.height * 0.60,
+        child: ListView.builder(
+            controller: _scrollController,
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return CachedNetworkImage(
+                imageUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+                imageBuilder: (con5text, imageProvider) {
+                  return Container(
+                    margin: const EdgeInsets.only(
+                      right: 8.0,
+                      left: 8.0,
+                      top: 10.0,
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.cover
+                      )
+                    ),
+
+
+                  );
+                },
+              );
+            }),
+      ),
     );
   }
 }
